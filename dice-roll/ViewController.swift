a2da610f34c3fcd2223db0388f71fc9344922e2a
Dileesha <cobsccomp192p-056@student.nibm.lk>
2021-02-23 07:03:14 +0800

    Initial Commit

---
+++ dice-roll/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dice-roll
+//
+//  Created by user191668 on 2/23/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
